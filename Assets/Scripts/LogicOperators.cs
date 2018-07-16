6e255553eed3f5e1edae2b3c96acc5c6d71c8a17
Jakub Dockal <jakub.dockal@gmail.com>
2018-07-16 15:01:47 +0200

    first commit git ignore

---
+++ Assets/Scripts/LogicOperators.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicOperators : MonoBehaviour {
+
+	public int A;
+	public int B;
+	public int Average;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Average = (A + B) / 2;
+		Debug.Log(Average);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Test01.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour {
+
+	public int MyHealth = 100;
+	public bool Alive = false;
+
+	// Use this for initialization
+	void Start()
+	{
+		if (MyHealth == 0)
+		{
+			Alive = false;
+		}
+		else if (MyHealth < 0)
+		{
+			Alive = false;
+		}
+		else
+		{
+			Alive = true;
+		}
+
+		if (Alive == true)
+		{
+			Debug.Log("I'm alive.");
+		}
+		else
+		{
+			Debug.Log("Sadly, I'm no longer alive.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
