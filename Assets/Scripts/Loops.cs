6d576b7f6d8514de73d88c037c3b7b869f1a601d
Jakub Dockal <jakub.dockal@gmail.com>
2018-07-17 09:54:49 +0200

    loops

---
+++ Assets/Scripts/Loops.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	public int[] Numbers = {5, 6, 7, 8};
+
+	// Use this for initialization
+	void Start ()
+	{
+		foreach (var number in Numbers)
+		{
+			Debug.Log(number);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
