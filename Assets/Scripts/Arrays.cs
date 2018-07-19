246f8b791794cdc4dfc6126cc690571e7425532f
Jakub Dockal <jakub.dockal@gmail.com>
2018-07-19 09:28:30 +0200

    another lesson

---
+++ Assets/Scripts/Arrays.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+
+	public GameObject[] myObjects;
+
+	// Use this for initialization
+	void Start () {
+		foreach (var gameObject in myObjects)
+		Debug.Log(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
